a002045ee4691ef26163c3112aba57181ff029a7
Amanpreet <aman11865@gmail.com>
2023-04-19 17:58:08 +0530

    fourth commit

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const router= express.Router()
-const contact= require('../controllers/contact')
+//const contact= require('../controllers/contact')
 const {authenticateUser,authorizePermissions}=require('../middleware/authentications')
-router.post('/message',authenticateUser,contact)
+//router.post('/message',authenticateUser,contact)
 
 module.exports= router 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,18 @@
 const express= require('express')
 const router= express.Router()
-const {signup,login,changePassword,deleteUser,verifyEmail,setForgotPassword,forgotPassword,getAllUsers}= require('../controllers/user')
+//const {signup,login,changePassword,deleteUser,verifyEmail,setForgotPassword,forgotPassword,getAllUsers}= require('../controllers/user')
+const {signup,login}= require('../controllers/user')
 
-const {authenticateUser,authorizePermissions}=require('../middleware/authentications')
+//const {authenticateUser,authorizePermissions}=require('../middleware/authentications')
 
 router.post('/signup',signup)
 router.post('/login',login)
-router.patch('/changePassword',authenticateUser,changePassword)
+/*router.patch('/changePassword',authenticateUser,changePassword)
 router.delete('/deleteUser',authenticateUser,deleteUser)
 router.patch('/verifyEmail',verifyEmail)
 router.patch('/forgotPassword',forgotPassword)
 router.patch('/setForgotPassword',setForgotPassword)
-router.get('/getAllUsers',authenticateUser,authorizePermissions,getAllUsers)
+router.get('/getAllUsers',authenticateUser,authorizePermissions,getAllUsers)*/
 
 
 module.exports= router
